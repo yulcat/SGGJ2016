d5abacd1dac62207d891dfebfa632c8e1de4b129
yulcat <alszmtm@gmail.com>
2016-10-29 02:38:41 +0900

    initial

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public struct XY
+{
+	public int x;
+	public int y;
+	public XY(int newX, int newY)
+	{
+		x = newX;
+		y = newY;
+	}
+	public Vector3 ToVector3()
+	{
+		var vec = new Vector3(x,y,0);
+		return vec/2f;
+	}
+}
+public class Block : MonoBehaviour
+{
+	List<Block> Feet;
+	Pyramid pyramid;
+	public XY position;
+
+	public void SetPyramid(Pyramid m)
+	{
+		pyramid = m;
+		var floatPos = transform.position * 2f;
+		position = new XY(Mathf.RoundToInt(floatPos.x), Mathf.RoundToInt(floatPos.y));
+	}
+
+	public void RefreshPosition()
+	{
+		RefreshPositionSelf(position);
+	}
+
+    void RefreshPositionSelf(XY targetPosition)
+    {
+		if(targetPosition.x == 0 && targetPosition.y == 1) return;
+		if(targetPosition.y == 1)
+		{
+			FallOff();
+		}
+		else if(pyramid.HasFeet(targetPosition))
+		{
+			FallTo(targetPosition);
+		}
+		else
+		{
+			targetPosition.y -= 2;
+			RefreshPositionSelf(targetPosition);
+		}
+    }
+	void FallTo(XY targetPosition)
+	{
+		transform.DOMove(targetPosition.ToVector3(), 0.5f).SetEase(Ease.InQuint);
+		position = targetPosition;
+	}
+	void FallOff()
+	{
+		transform.DOMoveY(-1, 0.5f).SetEase(Ease.OutQuint).OnComplete(() => Destroy(gameObject));
+	}
+    void OnMouseDown()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Pyramid.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Pyramid : MonoBehaviour {
+	List<Block> blocks;
+	// Use this for initialization
+	void Start () {
+		blocks = GetComponentsInChildren<Block>().ToList();
+		blocks.ForEach(b => b.SetPyramid(this));
+	}
+
+	public void RemoveBlock(Block block)
+	{
+		blocks.Remove(block);
+		blocks.ForEach(b => b.RefreshPosition());
+	}
+
+	public List<Block> FindFeet(XY pos)
+	{
+		return blocks.Where(b => CheckFeet(pos, b.position)).ToList();
+	}
+
+	public bool HasFeet(XY pos)
+	{
+		return blocks.Any(b => CheckFeet(pos, b.position));
+	}
+
+	public bool CheckFeet(XY pos, XY check)
+	{
+		return (check.y != pos.y - 2)
+			&& (check.x >= pos.x - 1)
+			&& (check.x <= pos.x + 1); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
