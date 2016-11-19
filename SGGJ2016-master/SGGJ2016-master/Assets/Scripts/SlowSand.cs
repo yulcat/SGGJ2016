d19e54aafc985a1f09ccb3f07c7fe63916d90f5f
yulcat <alszmtm@gmail.com>
2016-11-19 17:56:20 +0900

    프로젝트 하나로 합침.

--- SGGJ2016-master/SGGJ2016-master/Assets/Scripts/SlowSand.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class SlowSand : MonoBehaviour {
-	public float damp = 0.5f;
-	void OnTriggerStay (Collider col) {
-		col.GetComponent<Collider>().GetComponent<Rigidbody>().velocity *= 1-damp;
-	}
-}
