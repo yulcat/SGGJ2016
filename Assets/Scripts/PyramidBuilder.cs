be33e578a7f38e94c28a06ad46b666b4b0ec7de9
yulcat <alszmtm@gmail.com>
2016-11-23 22:55:55 +0900

    스테이지 생성을 위해 블록들을 Resources 안으로 옮김.

--- Assets/Scripts/PyramidBuilder.cs
+++ Assets/Scripts/PyramidBuilder.cs
@@ -2,12 +2,12 @@
 using System.Collections;
 using LitJson;
 
-public class PyramidMaker : MonoBehaviour {
-	public string stageName;
+public class PyramidBuilder : MonoBehaviour {
+	public string stageToLoad;
 
 	// Use this for initialization
 	void Start () {
-		var stage = Resources.Load<TextAsset>(stageName);
+		var stage = Resources.Load<TextAsset>(stageToLoad);
 		if(!stage) return;
 		// JsonMapper.ToObject(stage);
 	}
