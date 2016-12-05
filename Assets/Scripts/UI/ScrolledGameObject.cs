dabd832a86b1091b5f2059b4aebefc52b6792293
yulcat <alszmtm@gmail.com>
2016-12-06 01:25:22 +0900

    3d 박스가 스크롤링되도록 수정. 아직 버튼으로 동작 안 함.

---
+++ Assets/Scripts/UI/ScrolledGameObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrolledGameObject : MonoBehaviour {
+	public float scrollRatio = 0.004f;
+	public float deltaX = 30;
+	InfiniteScroll scroll;
+	int delta;
+
+	// Use this for initialization
+	void Start () {
+		scroll = FindObjectOfType<InfiniteScroll>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = scroll.xPosition * scrollRatio;
+		var pos = transform.position;
+		pos.x = x % deltaX;
+		transform.position = pos;
+	}
+}
