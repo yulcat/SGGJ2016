f5fbab4147b1b6d415052065f8717bbd5a44e1a3
yulcat <alszmtm@gmail.com>
2016-12-07 02:00:53 +0900

    스테이지 블럭들이 스크롤되고, 일단은 누르면 바로 스테이지 이동하도록 구현

--- Assets/Scripts/UI/LoadStageButton.cs
+++ Assets/Scripts/UI/LoadStageButton.cs
@@ -1,31 +1,10 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
+
 
 public class LoadStageButton : MonoBehaviour {
-	int _index;
-	public int index
-	{
-		get
-		{
-			return _index;
-		}
-		set
-		{
-			SetIndex(value);
-		}
-	}
-	void Start()
-	{
-		SetIndex(transform.GetSiblingIndex());
-	}
+	public int index;
 
-	void SetIndex(int i)
-	{
-		_index = i;
-		GetComponentInChildren<Text>().text = (_index+1).ToString();
-	}
-
-	public void LoadStage()
+	void OnMouseDown()
 	{
 		var toLoad = index+1;
 		StageManager.LoadStage(toLoad);
