7e26dfb60e32b09521948930275f50268f135d23
yulcat <alszmtm@gmail.com>
2016-12-14 21:54:35 +0900

    csv 파일 로드하는 스크립트 추가.

---
+++ Assets/Scripts/UI/MessageData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageData : MonoBehaviour {
+	public struct MessageBox
+	{
+		public string message;
+	}
+	static Dictionary<string, MessageBox> _dictionary;
+	public static Dictionary<string, MessageBox> dictionary
+	{
+		get
+		{
+			if(_dictionary == null)
+			{
+				var text = Resources.Load<TextAsset>("Data/messages");
+				_dictionary = CSVLoader.ToDictionary<MessageBox>(text.text);
+			}
+			return _dictionary;
+		}
+	}
+}
