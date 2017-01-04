fdea50528fa75b80554650b66f28fb786651bea3
yulcat <alszmtm@gmail.com>
2017-01-04 20:26:25 +0900

    카메라 세팅용 스크립트 추가. 플레이될때는 자동으로 비활성화됨

---
+++ Assets/Scripts/Utilities/CameraSetter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class CameraSetter : MonoBehaviour {
+	public int startingNumber = 3;
+	public int endingNumber = 9;
+	Camera[] cameras;
+	public bool run = false;
+	void OnDrawGizmos()
+	{
+		if(!run) return;
+		cameras = GetComponentsInChildren<Camera>();
+		if(cameras.Length != endingNumber - startingNumber + 1)
+			Debug.LogError("Camera count != camera numbers");
+		var initial = cameras[0].transform;
+		var ending = cameras[cameras.Length-1].transform;
+		for(int i=0; i<cameras.Length; i++)
+		{
+			var rate = (float)i/(cameras.Length-1);
+			cameras[i].transform.position = Vector3.Lerp(initial.position, ending.position, rate);
+			cameras[i].transform.rotation = Quaternion.Lerp(initial.rotation, ending.rotation, rate);
+		}
+		foreach(var c in cameras)
+		{
+			c.depth = -1;
+		}
+		if(cameras.Any(c => c.gameObject == Selection.activeGameObject))
+		{
+			cameras.First(c => c.gameObject == Selection.activeGameObject).depth = 0;
+		}
+	}
+
+	void Start()
+	{
+		cameras = GetComponentsInChildren<Camera>();
+		gameObject.SetActive(false);
+	}
+
+	public Transform GetProperTransform(int height)
+	{
+		return cameras[height+startingNumber].transform;
+	}
+}
