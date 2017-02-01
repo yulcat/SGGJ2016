67f830f1441512f93a3bd0e0f6429d355e45f076
yulcat <alszmtm@gmail.com>
2017-02-01 22:43:30 +0900

    목표달성 실패시에 목표달성실패 메시지를 띄워줌. (풍선 메시지 대신)

--- Assets/Scripts/UI/MessageData.cs
+++ Assets/Scripts/UI/MessageData.cs
@@ -3,19 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MessageData : MonoBehaviour {
-	public struct MessageBox
-	{
-		public string message;
-	}
-	static Dictionary<string, MessageBox> _dictionary;
-	public static Dictionary<string, MessageBox> dictionary
+	static Dictionary<string, string> _dictionary;
+	public static Dictionary<string, string> dictionary
 	{
 		get
 		{
 			if(_dictionary == null)
 			{
 				var text = Resources.Load<TextAsset>("Data/messages");
-				_dictionary = CSVLoader.ToDictionary<MessageBox>(text.text);
+				_dictionary = CSVLoader.ToDictionary<string>(text.text);
 			}
 			return _dictionary;
 		}
