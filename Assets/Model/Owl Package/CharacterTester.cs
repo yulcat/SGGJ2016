3c0d9c704c511186d6391d702f246a4f38d32819
lee <ljw3728@gmail.com>
2017-03-08 20:53:54 +0900

    보라색 새 추가!!

---
+++ Assets/Model/Owl Package/CharacterTester.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterTester : MonoBehaviour {
+
+	public Animator character_animator;
+	public Texture[] shirt_texture;
+	public Material shirt_material;
+	public GameObject shirt_mesh;
+
+	// Use this for initialization
+	void Start () 
+	{
+		character_animator = GetComponent<Animator> ();
+	}
+	
+	public void ShowShirt(int shirt_id)
+	{
+		shirt_mesh.SetActive (true);
+		shirt_material.SetTexture ("_MainTex", shirt_texture [shirt_id]);
+	}
+
+	public void NoShirt()
+	{
+		shirt_mesh.SetActive (false);
+	}
+
+	public void PlayAnimation(string trigger_name)
+	{
+		character_animator.SetTrigger (trigger_name);
+	}
+}
---
+++ Assets/Model/Owl Package/RotateScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+	public float rotation_speed = 5f;
+	Transform owl_transform;
+
+	// Use this for initialization
+	void Start () 
+	{
+		owl_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		owl_transform.RotateAround (Vector3.up, rotation_speed * Time.deltaTime);
+	}
+}
