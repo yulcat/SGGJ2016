57d177cf2a852019830273b9a3e6070aad9429de
yulcat <alszmtm@gmail.com>
2017-04-09 12:28:52 +0900

    창띄울때 계속 새창을 생성하지 말고 기존의 창 활용하는걸로

--- Assets/Scripts/UI/Windows/WindowHeartInsufficient.cs
+++ Assets/Scripts/UI/Windows/WindowHeartInsufficient.cs
@@ -6,6 +6,12 @@ public class WindowHeartInsufficient : Window {
 	public static void Open()
 	{
 		var canvas = FindObjectOfType<Canvas>();
+		var instanciated = canvas.GetComponentInChildren<WindowHeartInsufficient>(true);
+		if(instanciated != null)
+		{
+			instanciated.OpenWindow();
+			return;
+		}
 		var heartWindow = Instantiate<GameObject>(Resources.Load<GameObject>("UI/Window_notice_Heart"));
 		heartWindow.transform.SetParent(canvas.transform,false);
 		heartWindow.GetComponent<WindowHeartInsufficient>().OpenWindow();
