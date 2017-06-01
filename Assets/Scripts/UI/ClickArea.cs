5ae6b2413bbf032d474c961264047953f6a45426
yulcat <alszmtm@gmail.com>
2017-06-02 00:05:23 +0900

    튜토리얼 UI 작업중

---
+++ Assets/Scripts/UI/ClickArea.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class ClickArea : UIBehaviour, IPointerClickHandler, ICanvasRaycastFilter {
+	public UnityEvent OnClick;
+
+    public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+    {
+		Debug.Log(sp);
+        return true;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+		Debug.Log("Click");
+        OnClick.Invoke();
+    }
+}
