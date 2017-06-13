16f35f7a8a8d3239d572e79b549b1fe4d4d9d5da
jongwonIee <ljw3728@gmail.com>
2017-06-13 07:24:41 -0800

    fb login / share

--- Assets/Scripts/UI/Title.cs
+++ Assets/Scripts/UI/Title.cs
@@ -2,15 +2,19 @@
 using System.Collections;
 
 public class Title : MonoBehaviour {
-	IEnumerator Start () {
-		// QualitySettings.vSyncCount = 0;
-		// Application.targetFrameRate = 30;
-		// yield return new WaitForSeconds(2);
-		while(true)
-		{
-			if(Input.GetMouseButtonDown(0))
-				SceneLoader.LoadScene(1);
-			yield return null;
-		}
-	}
+//	IEnumerator Start () {
+//		// QualitySettings.vSyncCount = 0;
+//		// Application.targetFrameRate = 30;
+//		// yield return new WaitForSeconds(2);
+//		while(true)
+//		{
+//			if(Input.GetMouseButtonDown(0))
+//				SceneLoader.LoadScene(1);
+//			yield return null;
+//		}
+//	}
+    public void StartWithoutLogin()
+    {
+        SceneLoader.LoadScene(1);
+    }
 }
