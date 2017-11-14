b6b9e73607084281d213d9eed1afae7ac2a1d588
yulcat <alszmtm@gmail.com>
2017-11-14 21:36:54 +0900

    스플래시 -> 스테이지 셀렉트 넘어가도록 적용

---
+++ Assets/Scripts/SplashFade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class SplashFade : MonoBehaviour
+{
+    static bool isFirstTime = true;
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        if (!isFirstTime)
+        {
+            this.gameObject.SetActive(false);
+            yield break;
+        }
+        isFirstTime = false;
+        var child = transform.GetChild(0);
+        child.gameObject.SetActive(true);
+        var img = child.GetComponent<Image>();
+        yield return new WaitForSeconds(0.1f);
+        img.DOColor(Color.clear, 0.5f).OnComplete(() => gameObject.SetActive(false));
+    }
+}
