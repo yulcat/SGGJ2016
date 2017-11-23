39fd633c30956b5e08550f79c280e9a275fa82eb
yulcat <alszmtm@gmail.com>
2017-11-23 21:54:53 +0900

    캐릭터 선택창 추가 (아직 정상작동 안함)

---
+++ Assets/Scripts/UI/CharacterSelect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public GameObject[] characters;
+    public GameObject cursor;
+    public void SelectCharacter(int index)
+    {
+        GameState.SelectedCharacter = (CharacterType)index;
+        cursor.transform.DOMove(characters[index].transform.position, 0.3f, true).SetEase(Ease.OutBack);
+    }
+
+}
