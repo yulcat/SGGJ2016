2b46f95ff9c8700e4b427ac858c2da2478808ff4
yulcat <alszmtm@gmail.com>
2017-11-28 22:03:52 +0900

    캐릭터 구매 프로세스 구현완료

--- Assets/Scripts/Editor/SaveFileModifier.cs
+++ Assets/Scripts/Editor/SaveFileModifier.cs
@@ -4,32 +4,69 @@ using UnityEngine;
 using UnityEditor;
 using System.Linq;
 
-public class SaveFileModifier : Editor {
-	[MenuItem("Tools/Edit Save File/Clear All Stages")]
-	static void ClearAll () {
-		var max = StageDataLoader.GetMaxStage();
-		for(int i=0; i<max; i++)
-		{
-			if(SaveDataManager.clearRecord.ContainsKey(i.ToString()))
-				continue;
-			var clear = new SaveDataManager.ClearData();
-			SaveDataManager.clearRecord.Add(i.ToString(),clear);
-		}
-		SaveDataManager.Save();
-	}
-	
-	[MenuItem("Tools/Edit Save File/Delete Save Data")]
-	static void Delete () {
-		SaveDataManager.clearRecord.Clear();
-		SaveDataManager.Save();
-	}
+public class SaveFileModifier : Editor
+{
+    [MenuItem("Tools/Edit Save File/Clear All Stages")]
+    static void ClearAll()
+    {
+        var max = StageDataLoader.GetMaxStage();
+        for (int i = 0; i < max; i++)
+        {
+            if (SaveDataManager.clearRecord.ContainsKey(i.ToString()))
+                continue;
+            var clear = new SaveDataManager.ClearData();
+            SaveDataManager.clearRecord.Add(i.ToString(), clear);
+        }
+        SaveDataManager.Save();
+    }
 
-	[MenuItem("Tools/Edit Hearts/Zero Heart")]
-	static void ZeroHeart () {
-		var hearts = SaveDataManager.data.heartLeft;
-		for(int i=0; i<hearts; i++)
-		{
-			HeartManager.SpendHeart();
-		}
-	}
+    [MenuItem("Tools/Edit Save File/Delete Save Data")]
+    static void Delete()
+    {
+        SaveDataManager.clearRecord.Clear();
+        SaveDataManager.Save();
+    }
+
+    [MenuItem("Tools/Edit Hearts/Zero Heart")]
+    static void ZeroHeart()
+    {
+        var hearts = SaveDataManager.data.heartLeft;
+        for (int i = 0; i < hearts; i++)
+        {
+            HeartManager.SpendHeart();
+        }
+    }
+
+    [MenuItem("Tools/Edit Save File/Enable all Characters")]
+    static void EnableCharacters()
+    {
+        SaveDataManager.UnlockCharacter(1);
+        SaveDataManager.UnlockCharacter(2);
+    }
+
+    [MenuItem("Tools/Edit Save File/Disable all Characters")]
+    static void DisableCharacters()
+    {
+        SaveDataManager.data.unlockedCharacters = 0;
+        SaveDataManager.Save();
+    }
+
+    [MenuItem("Tools/Edit Save File/Add Stars")]
+    static void AddStars()
+    {
+        SaveDataManager.DebugAddStar(30);
+    }
+
+    [MenuItem("Tools/Edit Save File/Reset Stars")]
+    static void ResetStars()
+    {
+        SaveDataManager.data.spentStars = 0;
+        SaveDataManager.Save();
+    }
+
+    [MenuItem("Tools/Edit Save File/Reload DB")]
+    static void ReloadDB()
+    {
+        PlayerPrefs.DeleteKey("downloadTime");
+    }
 }
--- Assets/Scripts/UI/CharacterSelect.cs
+++ Assets/Scripts/UI/CharacterSelect.cs
@@ -2,23 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
+using UnityEngine.UI;
 
 public class CharacterSelect : MonoBehaviour
 {
     public GameObject[] characters;
     public GameObject cursor;
+    Action unlockResult;
     void OnEnable()
     {
-        for (int i = 0; i < characters.Length; i++)
+        for (int i = 1; i < characters.Length; i++)
         {
-            if (!SaveDataManager.IsCharacterAvailable(i)) continue;
+            var available = SaveDataManager.IsCharacterAvailable(i);
+            var charImg = characters[i].transform.GetChild(0).GetComponent<Image>();
+            var lockImg = characters[i].transform.GetChild(1).gameObject;
+            charImg.color = available ? Color.white : Color.black;
+            lockImg.SetActive(!available);
+        }
+    }
+
+    void Update()
+    {
+        if (unlockResult != null)
+        {
+            unlockResult();
+            unlockResult = null;
         }
     }
 
     public void SelectCharacter(int index)
     {
+        if (!SaveDataManager.IsCharacterAvailable(index))
+        {
+            TryUnlockCharacter(index);
+            return;
+        }
         GameState.SelectedCharacter = (CharacterType)index;
         cursor.transform.DOMove(characters[index].transform.position, 0.3f, true).SetEase(Ease.OutBack);
     }
 
+    private void TryUnlockCharacter(int index)
+    {
+        WindowYNPop.OpenYNPop(string.Format(DB.MessageDB["charBuy_confirm"], DB.characterDB[index].name, DB.characterDB[index].price), () => UnlockSuccess(index));
+    }
+
+    private void UnlockSuccess(int index)
+    {
+        var success = SaveDataManager.BuyCharacter(index);
+        unlockResult = () => WindowPop.Open(success ? DB.MessageDB["charBuy_success"] : DB.MessageDB["charBuy_failed"]);
+    }
 }
