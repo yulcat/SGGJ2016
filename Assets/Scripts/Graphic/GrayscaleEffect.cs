573f4194e4fc865889916378271a9b50eff95f5f
yulcat <alszmtm@gmail.com>
2018-02-06 22:35:04 +0900

    그레이스케일 이펙트 추가. 아직 마스킹 작동 안함

---
+++ Assets/Scripts/Graphic/GrayscaleEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class GrayscaleEffect : MonoBehaviour
+{
+    public Material material;
+
+//    void OnEnable()
+//    {
+//        material = new Material(Shader.Find("Hidden/GrayscaleFromPoint"));
+//    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, material);
+    }
+}
