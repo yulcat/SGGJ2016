e595be4c07fc28b6ec32f3021b4c904e60f25f7b
yulcat <alszmtm@gmail.com>
2018-07-09 22:33:43 +0900

    피라미드 붕괴하면 그림자 투명해짐

---
+++ Assets/Scripts/Graphic/OccluderOnOff.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class OccluderOnOff : MonoBehaviour
+{
+    Pyramid nabla;
+
+    Renderer occluderRenderer;
+    float defaultAlpha;
+
+    // Use this for initialization
+    void Start()
+    {
+        nabla = FindObjectOfType<Pyramid>();
+        occluderRenderer = GetComponentInChildren<Renderer>();
+        defaultAlpha = occluderRenderer.material.GetFloat("_Alpha");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        occluderRenderer.material.SetFloat("_Alpha", nabla.IsCollapsed() ? 0f : 0.681f);
+    }
+}
